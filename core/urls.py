cd4401d4f4b7cc624fbe246d83a5092a427a152f
vitoananda <vtoananda@gmail.com>
2023-01-01 00:45:12 +0700

    added login, profile page, and logout features

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index,name='index'),
     path('about', views.about,name='about'),
-    path('contact', views.contact,name='contact')
+    path('contact', views.contact,name='contact'),
+    path('accounts/profile', views.ProfileView.as_view(),name='profile'),
+
+    #django auth
+    path('accounts/login',auth_views.LoginView.as_view(template_name="accounts/login.html"),name="login"),
+    path('accounts/logout',auth_views.LogoutView.as_view(),name="logout"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def index(request):
     return render(request, 'index.html')
@@ -10,3 +12,5 @@ def about(request):
 def contact(request):
     return render(request, 'contact.html')
 
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/profile.html'
